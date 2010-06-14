d22fa75fd3d6b7024e2ce0089d850d47a6dfddd0
Daniel Poelzleithner <git@poelzi.org>
2010-06-15 00:42:44 +0200

    fix merge error

--- intrinsics.h
+++ intrinsics.h
@@ -9,20 +9,6 @@ typedef unsigned short istate_t;
 extern "C"
 {
 #endif
-<<<<<<< HEAD
-        typedef unsigned short istate_t;
-
-	unsigned short __get_interrupt_state(void);
-	void     __set_interrupt_state(unsigned short);
-	
-	/* __even_in_range is used only in a switch statement __bound parameter must be a literal */
-	unsigned short __even_in_range(unsigned short __value,
-                                  unsigned short __bound);
-								  
-	/* Insert a delay with a specific number of cycles. */
-	void __delay_cycles(unsigned long __cycles);
-=======
-
 
   istate_t __get_interrupt_state(void);
   void     __set_interrupt_state(istate_t);
@@ -32,8 +18,6 @@ extern "C"
   /* Insert a delay with a specific number of cycles. */
   void __delay_cycles(unsigned long __cycles);
 
->>>>>>> 312f9be6fef25352922d4c531b3591007c404905
-  
 #ifdef __cplusplus
 }
 #endif
