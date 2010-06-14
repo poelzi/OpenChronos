eedb459b3d75d85c287f0c5af105de68677aa7b2
Paul F. Sehorne <Paul@Sehorne.org>
2010-06-14 15:12:30 -0500

    For consistency with TI source, added type def for istate_t and am using it in the __get_interrupt_state and __set_interrup_stata functions that follow

--- intrinsics.h
+++ intrinsics.h
@@ -1,23 +1,24 @@
-
 #ifndef __INTRINSICS_H
 #define __INTRINSICS_H
 
 #include <signal.h>
 
+typedef unsigned short istate_t;
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-	unsigned short __get_interrupt_state(void);
-	void     __set_interrupt_state(unsigned short);
-	
-	/* __even_in_range is used only in a switch statement __bound parameter must be a literal */
-	unsigned short __even_in_range(unsigned short __value,
-                                  unsigned short __bound);
-								  
-	/* Insert a delay with a specific number of cycles. */
-	void __delay_cycles(unsigned long __cycles);
+
+
+  istate_t __get_interrupt_state(void);
+  void     __set_interrupt_state(istate_t);
+  unsigned short __even_in_range(unsigned short __value,
+                                             unsigned short __bound);
+
+  /* Insert a delay with a specific number of cycles. */
+  void __delay_cycles(unsigned long __cycles);
+
   
 #ifdef __cplusplus
 }
